2e6ab22dce7df030a45c87d8d5d049493a2c2480
Marc Brinkmann <marc@casperlabs.io>
2022-01-31 18:45:42 +0100

    Finish first version that correctly writes to shack

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use std::path::{Path, PathBuf};
+use std::{
+    collections::HashMap,
+    ops::Add,
+    path::{Path, PathBuf},
+};
 
+use generic_array::typenum;
 use indicatif::ProgressIterator;
 use lmdb::{Cursor, Environment, EnvironmentFlags, Transaction};
 use structopt::StructOpt;
+use stuffer_shack::StufferShack;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
     /// Path to LMDB database.
     input_db: PathBuf,
+
+    /// Stuffer shack to output to.
+    #[structopt(long, short)]
+    output: Option<PathBuf>,
+
+    /// Key size to use
+    #[structopt(long, short, default_value = "32")]
+    key_size: u16,
 
     /// Database to copy.
     #[structopt(long, short)]
@@ -41,7 +55,15 @@ fn main() {
     let entry_count = env.stat().unwrap().entries();
     println!("number of entries in all dbs: {:?}", entry_count);
 
-    if let Some(ref db_name) = opts.db_name {
+    if let (Some(ref db_name), Some(ref output)) = (opts.db_name, opts.output) {
+        assert!(!output.exists());
+
+        let mut shack: StufferShack<typenum::U32> =
+            StufferShack::open_disk(output).expect("could not open stuffer shack");
+
+        let mut key_lens: HashMap<usize, u64> = HashMap::new();
+        let mut value_lens: HashMap<usize, u64> = HashMap::new();
+
         env.begin_ro_txn()
             .unwrap()
             .get(root_db, db_name)
@@ -54,9 +76,20 @@ fn main() {
         let txn = env.begin_ro_txn().unwrap();
         let db_iter = txn.open_ro_cursor(root_db).unwrap().iter_start();
         for (key, value) in db_iter.progress_count(entry_count as u64) {
-            //  println!("{:?} {:?}", key, value);
+            // Count entries for stats.
+            *key_lens.entry(key.len()).or_default() += 1;
+            *value_lens.entry(value.len()).or_default() += 1;
+
+            // Write to shack, padding or shortening the key.
+            let mut new_key = [0xFFu8; 32];
+            let l = key.len().min(32);
+            new_key[..l].copy_from_slice(&key[..l]);
+            shack.write(new_key.into(), value);
         }
 
         println!("done");
+
+        println!("keys lengths: {:#?}", key_lens);
+        println!("value lengths: {:#?}", value_lens);
     }
 }
